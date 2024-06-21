6873b37110594bd36a0107216122024b8d750a00
Jhony Vidal <jhonylopez147@hotmail.com>
2024-06-21 12:14:25 -0500

    last changes

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -25,33 +25,45 @@ export default class RegisterComponent {
     private userService: UserService,
     private router: Router
   ) {
-    this.registerForm = this.fb.group({
-      name: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      passwordHash: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-      terms: [false, Validators.requiredTrue]
-    },{ Validators: passwordsMatchValidator });
+    this.registerForm = this.fb.group(
+      {
+        name: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        passwordHash: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+        terms: [false, Validators.requiredTrue],
+      },
+      { Validators: passwordsMatchValidator }
+    );
   }
 
   isFieldInvalid(field: string): boolean {
     const control = this.registerForm.get(field);
-    return control ? control.invalid && (control.dirty || control.touched) : false;
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
   }
 
   onSubmit(event: Event) {
     event.preventDefault();
     if (this.registerForm.valid) {
-      this.userService
-      .post(this.registerForm.value)
-      .subscribe((response) => {
-        if (response) {
-          this.router.navigate(['/home']);
-        } else {
-          alert('Error de autenticación');
+      this.userService.post(this.registerForm.value).subscribe(
+        (response) => {
+          if (response) {
+            this.router.navigate(['/home']);
+          } else {
+            alert('Ha ocurrido un error');
+          }
+        },
+        (error) => {
+          if(error.error.Message === "Email address is already registered."){
+            alert('El correo ya se encuentra registrado.');
+          }else{
+            alert('Ha ocurrido un error.');
+          }
         }
-      });
+      );
     }
   }
 }
